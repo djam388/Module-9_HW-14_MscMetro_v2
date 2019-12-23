e8d2a1e52967348b080545d8e7d99c602d478580
djam388 <djam388@hotmail.com>
2019-12-23 16:54:45 +0500

    Moscow Metro parsing added

---
+++ src/main/java/app/Serialize.java
@@ -0,0 +1,289 @@
+package app;
+
+import model.Line;
+import model.Station;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import utils.StationIndex;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Serialize
+{
+    private static ArrayList<Line> sortedLinesLA = new ArrayList<>();
+    public static void main(String[] args)
+    {
+
+        Map<Double, Line> lines = new TreeMap<>();
+        ArrayList<Line> sortedLinesLA = new ArrayList<>();
+        ArrayList<String> listStations = new ArrayList<>();
+        Map<String, List<String>> connectionsFlat = new HashMap<>();
+        Double lineNumberInDouble = 0.0;
+        StationIndex stationIndex = new StationIndex();
+
+        String htmlString = null;
+        try {
+            htmlString = Jsoup.connect("https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0")
+                    .maxBodySize(0)
+                    .get()
+                    .html();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Document doc = Jsoup.parse(htmlString);
+        for (int tbl = 3; tbl < 6; tbl++) {
+            Element table = doc.select("tbody").get(tbl);
+            Elements rows = table.select("tr");
+            int rowNumber;
+            if (tbl == 3)
+            {
+                rowNumber = 1;
+            }
+            else
+            {
+                rowNumber = 2;
+            }
+            for (int i = rowNumber; i < rows.size(); i++) {
+                boolean connectionFound = false;
+                Element row = rows.get(i);
+                Elements colLineDetails = row.select("td");
+                Elements connectedStations = colLineDetails.get(3).select("span");
+                if (connectedStations.size() > 0)
+                {
+                    connectionFound = true;
+                }
+                ArrayList<String> connectedStation = new ArrayList<>();
+                String stationInfo;
+                Line lineToCheck;
+
+
+                int foundIndex = colLineDetails.get(1).select("a").attr("title").indexOf("(") > 0 ?
+                        colLineDetails.get(1).select("a").attr("title").indexOf("(") :
+                        colLineDetails.get(1).select("a").attr("title").length();
+
+                if (!colLineDetails.get(0).text().contains(" ")) // проверка на присутствие другой линии для одной станции
+                { // если одна линия
+                    if (colLineDetails.get(0).text().substring(0, 1).equals("0")) // проверяем есть ли "0" перед номером линии
+                    { // если есть, убираем "0"
+                        lineNumberInDouble = convertLineNumberToDouble(colLineDetails.get(0).text().substring(1, (colLineDetails.get(0).text().length() - 2)));
+                        stationInfo = lineNumberInDouble.toString()
+                                + ", "
+                                + colLineDetails.get(1).select("a").attr("title").substring(0, foundIndex - 1);
+                        if (connectionFound)
+                        {
+                            connectionsFlat.putAll(addConnectedStationsFlat(stationInfo, connectedStations));
+                        }
+                        listStations.add(stationInfo); //формируется список станций с номером линии
+                        if (lines.containsKey(lineNumberInDouble)) //проверка на существовании линии в списке
+                        { //если есть, получаем данные для проверки цвета -> для коррекции
+                            lineToCheck = lines.get(lineNumberInDouble);
+                            if (lineToCheck.getColor().equals("#000000")) {//если не был определен цвет, меняем на правильный цвет
+                                lines.replace(lineNumberInDouble,
+                                        new Line(lineNumberInDouble,
+                                                colLineDetails.get(0).select("span").attr("title"),
+                                                (colLineDetails.get(0).attr("style").length() > 0 ? colLineDetails.get(0).attr("style").replaceAll(("background:"), "") : "#000000"))); // #000000 используется, если при парсинге не был обнаружен цвет
+                            }
+                        } else {//добавляем новую линию
+                            lines.put(lineNumberInDouble,
+                                    new Line(lineNumberInDouble,
+                                            colLineDetails.get(0).select("span").attr("title"),
+                                            (colLineDetails.get(0).attr("style").length() > 0 ? colLineDetails.get(0).attr("style").replaceAll(("background:"), "") : "#000000"))); // #000000 используется, если при парсинге не был обнаружен цвет
+                        }
+
+
+                    } else {//используем номер линии без корректировки
+
+                        lineNumberInDouble = convertLineNumberToDouble(colLineDetails.get(0).text().substring(0, (colLineDetails.get(0).text().length() - 2)));
+                        stationInfo = lineNumberInDouble
+                                + ", "
+                                + colLineDetails.get(1).select("a").attr("title").substring(0, foundIndex - 1);
+                        if (connectionFound)
+                        {
+                            connectionsFlat.putAll(addConnectedStationsFlat(stationInfo, connectedStations));
+                        }
+                        listStations.add(stationInfo); //формируется список станций с номером линии
+                        if (lines.containsKey(lineNumberInDouble)) //проверка на существовании линии в списке
+                        { // если есть, получаем данные для проверки цвета -> для коррекции
+                            lineToCheck = lines.get(lineNumberInDouble);
+                            if (lineToCheck.getColor().equals("#000000")) { // если не был определен цвет, меняем на правильный цвет
+                                lines.replace(lineNumberInDouble,
+                                        new Line(lineNumberInDouble,
+                                                colLineDetails.get(0).select("span").attr("title"),
+                                                (colLineDetails.get(0).attr("style").length() > 0 ? colLineDetails.get(0).attr("style").replaceAll(("background:"), "") : "#000000"))); // #000000 используется, если при парсинге не был обнаружен цвет
+                            }
+                        } else { // добавляем новую линию
+                            lines.put(lineNumberInDouble,
+                                    new Line(lineNumberInDouble,
+                                            colLineDetails.get(0).select("span").attr("title"),
+                                            (colLineDetails.get(0).attr("style").length() > 0 ? colLineDetails.get(0).attr("style").replaceAll(("background:"), "") : "#000000")));
+                        }
+
+                    }
+                } else { // если станция относится к двум линия, то разбиваем поле с двумя значениями на отдельные фрагменты
+                    String[] fragments = colLineDetails.get(0).text().split(" ");
+                    stationInfo = convertLineNumberToDouble(fragments[0]) + ", " + colLineDetails.get(1).select("a").attr("title").substring(0, foundIndex - 1);
+                    listStations.add(stationInfo);
+                    if (connectionFound)
+                    {
+                        connectionsFlat.putAll(addConnectedStationsFlat(stationInfo, connectedStations));
+                    }
+                    stationInfo = convertLineNumberToDouble(fragments[1].substring(0, 2)) + ", " + colLineDetails.get(1).select("a").attr("title").substring(0, foundIndex - 1);
+                    listStations.add(stationInfo);
+                    if (connectionFound)
+                    {
+                        connectionsFlat.putAll(addConnectedStationsFlat(stationInfo, connectedStations));
+                    }
+                }
+            }
+        }
+
+        Line linesToEdit;
+
+        for (String station : listStations) // добавляем станции к линиям соответственно
+        {
+            String[] fragments = station.split(", ");
+            linesToEdit = lines.get(Double.parseDouble(fragments[0]));
+            linesToEdit.addStation(new Station(fragments[1], lines.get(Double.parseDouble(fragments[0]))));
+
+        }
+
+        for (Double lineNumber : lines.keySet()) // добавление линии и станции в переменную stationIndex
+        {
+            sortedLinesLA.add(lines.get(lineNumber));
+            stationIndex.addLine(lines.get(lineNumber));
+            lines.get(lineNumber).getStations().forEach(station -> {
+                stationIndex.addStation(new Station(station.getName(), station.getLine()));
+            });
+        }
+
+        for (String station : connectionsFlat.keySet()) // добавление перехода в переменную stationIndex
+        {
+            ArrayList<Station> stations = new ArrayList<>();
+            //Station stationConnected = stationIndex.getStation(station);
+            stations.add(new Station(station.substring(station.indexOf(",") + 2),
+                            lines.get(Double.parseDouble(station.substring(0, station.indexOf(",") - 1)))));
+            for (String info : connectionsFlat.get(station))
+            {
+                //stationConnected = stationIndex.getStation(extractStationName(info, listStations));
+                if (info.equals("Переход на станцию Деловой центр Калининской линии")) // на Калининской линии нет станции Деловой центр
+                {
+                    //System.out.println("stop!");
+                    stations.add(extractStationName("Переход на станцию Деловой центр Солнцевской линии", lines));
+                }
+                else
+                {
+                    stations.add(extractStationName(info, lines));
+                }
+            }
+            stationIndex.addConnection(stations);
+        }
+
+
+        System.out.println("Parsing finished");
+    }
+
+    public static Map<String, List<String>> addConnectedStationsFlat(String stationInfo, Elements connectedStationsInfo)
+    {
+        ArrayList<String> addedConnectedStation = new ArrayList<>();
+        Map<String, List<String>> addedConnectionsFlat = new HashMap<>();
+        for (Element station : connectedStationsInfo) //предварительные данные для переходов между станциями
+        {
+
+            if (station.attr("title").length() > 0) {
+                addedConnectedStation.add(station.attr("title"));
+                //System.out.println(stationInfo + ": " + station.attr("title"));
+                //System.out.println(colLineDetails.get(1).select("a").attr("title").substring(0, foundIndex - 1)
+                //        + ": "
+                //        + station.attr("title"));
+            }
+            //connectionsFlat.put(colLineDetails.get(1).select("a").attr("title").substring(0, foundIndex - 1),
+            //    connectedStation);
+        }
+        addedConnectionsFlat.put(stationInfo, addedConnectedStation);
+        return addedConnectionsFlat;
+    }
+
+    public static Station extractStationName (String title, Map<Double, Line> stationsToCompare)
+    {
+        String dataToExtractLine = null;
+        String foundStation = null;
+        boolean searchCompleted = false;
+        for (Double stationNumber : stationsToCompare.keySet())
+        {
+            List<Station> stationNames = stationsToCompare.get(stationNumber).getStations();
+            for (Station station : stationNames){
+                Pattern pattern;
+                pattern = Pattern.compile("(" + "ю " + station.getName() + ")", Pattern.MULTILINE);
+                Matcher matcher = pattern.matcher(title);
+                boolean stationFound = (matcher.find());
+                if (stationFound)
+                {
+                    //System.out.println(title);
+                    foundStation = matcher.group(0);
+                    int startIndex = title.indexOf(matcher.group(0)) + foundStation.length();
+                    dataToExtractLine = title.substring(startIndex + 1).trim();
+                    //System.out.println(dataToExtractLine);
+                    if (dataToExtractLine.equals("Московского центрального кольца"))
+                    {
+                        dataToExtractLine = "Московское центральное кольцо";
+                    }
+                    else if (dataToExtractLine.equals("Московского монорельса"))
+                    {
+                        dataToExtractLine = "Московский монорельс";
+                    }
+                    else if (dataToExtractLine.equals("Большой кольцевой линии"))
+                    {
+                        dataToExtractLine = "Большая кольцевая линия";
+                    }
+                    else
+                    {
+                        dataToExtractLine = dataToExtractLine.substring(0, dataToExtractLine.indexOf("ой линии"))
+                                + "ая линия";
+                    }
+                    //Московского центрального кольца -> Московское центральное кольцо
+                    //Московского монорельса -> Московский монорельс
+                    //Большой кольцевой линии -> Большая кольцевая линия
+                    //Кольцевой линии -> Кольцевая линия
+                    //System.out.println(dataToExtractLine);
+                }
+            }
+        }
+
+
+        for (Double stationNumber : stationsToCompare.keySet())
+        {
+            if (stationsToCompare.get(stationNumber).getName().equals(dataToExtractLine))
+            {
+                for (Station station : stationsToCompare.get(stationNumber).getStations())
+                {
+                    if (station.getName().equals(foundStation.substring(2)))
+                    {
+                        return station;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static double convertLineNumberToDouble (String lineNumber)
+    {
+        Pattern pattern = Pattern.compile("\\D+", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(lineNumber);
+        boolean letterFound = (matcher.find());
+
+        if (!letterFound)
+        {
+            return Double.parseDouble(lineNumber);
+        }
+        else
+        {
+            return Double.parseDouble(lineNumber.substring(0, lineNumber.indexOf(matcher.group(0)))) + 0.5;
+        }
+    }
+}
